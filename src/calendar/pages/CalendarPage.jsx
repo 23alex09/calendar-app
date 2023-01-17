04281ecf3162ddc24aa76a9fcec589e184276ea4
Alejandro Hidalgo Acuña <23alex09@gmail.com>
2023-01-17 16:08:52 +0100

    Add NavBar.jsx and create the calendar

--- src/calendar/pages/CalendarPage.jsx
+++ src/calendar/pages/CalendarPage.jsx
@@ -1,5 +1,47 @@
+import { Calendar, dateFnsLocalizer } from 'react-big-calendar'
+import { addHours, format, parse, startOfWeek, getDay } from 'date-fns'
+import enUS from 'date-fns/locale/en-US'
+import 'react-big-calendar/lib/css/react-big-calendar.css'
+
+import { NavBar } from "../components"
+
+const locales = {
+    'en-US': enUS,
+}
+
+const localizer = dateFnsLocalizer( {
+    format,
+    parse,
+    startOfWeek,
+    getDay,
+    locales,
+} )
+
+const events = [ {
+    title: 'Cumpleaños del jefe',
+    notes: 'Comprar tarta',
+    start: new Date(),
+    end: addHours( new Date(), 2 ),
+    bgColor: '#fafafa',
+    user: {
+        _id: '123',
+        name: 'Alejandro'
+    }
+} ]
+
+
 export const CalendarPage = () => {
     return (
-        <div>CalendarPage</div>
+        <>
+            <NavBar />
+
+            <Calendar
+                localizer={ localizer }
+                events={ events }
+                startAccessor="start"
+                endAccessor="end"
+                style={ { height: 'calc(100vh - 80px)' } }
+            />
+        </>
     )
 }
