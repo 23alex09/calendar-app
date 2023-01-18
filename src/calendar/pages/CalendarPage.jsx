956fc84d3fdbee7cb81a0df0562b1c43aba09525
Alejandro Hidalgo Acuña <23alex09@gmail.com>
2023-01-18 08:42:33 +0100

    refactor CalendarPage code

--- src/calendar/pages/CalendarPage.jsx
+++ src/calendar/pages/CalendarPage.jsx
@@ -1,21 +1,10 @@
-import { Calendar, dateFnsLocalizer } from 'react-big-calendar'
-import { addHours, format, parse, startOfWeek, getDay } from 'date-fns'
-import enUS from 'date-fns/locale/en-US'
+import { Calendar } from 'react-big-calendar'
+import { addHours } from 'date-fns'
 import 'react-big-calendar/lib/css/react-big-calendar.css'
 
 import { NavBar } from "../components"
+import { getMessagesES, localizer } from '../../helpers'
 
-const locales = {
-    'en-US': enUS,
-}
-
-const localizer = dateFnsLocalizer( {
-    format,
-    parse,
-    startOfWeek,
-    getDay,
-    locales,
-} )
 
 const events = [ {
     title: 'Cumpleaños del jefe',
@@ -29,18 +18,36 @@ const events = [ {
     }
 } ]
 
-
 export const CalendarPage = () => {
+
+    const eventStyleGetter = ( event, start, end, isSelected ) => {
+        console.log( { event, start, end, isSelected } )
+
+        const style = {
+            backgroundColor: '#347CF7',
+            borderRadius: '0px',
+            opacity: 0.8,
+            color: 'white'
+        }
+
+        return {
+            style
+        }
+    }
+
     return (
         <>
             <NavBar />
 
             <Calendar
+                culture='es'
                 localizer={ localizer }
                 events={ events }
                 startAccessor="start"
                 endAccessor="end"
                 style={ { height: 'calc(100vh - 80px)' } }
+                messages={ getMessagesES() }
+                eventPropGetter={ eventStyleGetter }
             />
         </>
     )
